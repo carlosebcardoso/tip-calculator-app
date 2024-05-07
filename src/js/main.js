c4e548befec3b305cc87cc35e3956ec8e2bbd7ac
carlos.eduardo <carlosebcardoso@gmail.com>
2024-05-07 18:30:53 -0300

    html structure and initial button structure

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const tipButton = (text) => {
+    const button = document.createElement('button')
+    button.classList = "bg-very_dark_cyan text-white py-2 text-lg text-center w-full rounded-md"
+
+    return button
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Mono', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        "primary": "hsl(172, 67%, 45%)",
+        "very_dark_cyan": "hsl(183, 100%, 15%)",
+        "dark_cyan": "hsl(186, 14%, 43%)",
+        "gray_cyan": "hsl(184, 14%, 56%)",
+        "light_gray_cyan": "hsl(185, 41%, 84%)",
+        "very_light_gray_cyan": "hsl(189, 41%, 97%)"
+      }
+    }
   },
   plugins: [],
 }
