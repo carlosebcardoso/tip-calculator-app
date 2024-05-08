e29cf6a52ad656e67d61030cc45e682744faa885
carlos.eduardo <carlosebcardoso@gmail.com>
2024-05-08 18:27:38 -0300

    css update and auto generating buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 const tipButton = (text) => {
     const button = document.createElement('button')
-    button.classList = "bg-very_dark_cyan text-white py-2 text-lg text-center w-full rounded-md"
+    button.classList = "bg-very_dark_cyan text-white py-2 text-xl text-center rounded-md"
+    button.innerText = text
 
     return button
+}
+
+
+const tipButtons = document.getElementById('tip-buttons')
+const tipTexts = ['5%', '10%', '15%', '25%', '50%', 'Custom']
+
+for (let index = 0; index < tipTexts.length; index++) {
+    const text = tipTexts[index];
+    
+    const button = tipButton(text)
+
+    tipButtons.appendChild(button)
 }
