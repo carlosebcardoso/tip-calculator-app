4f0c6d2b0ec74f37ec3308e3fc7f2a32d9419d00
Doulpy <carlosebcardoso@gmail.com>
2024-05-09 11:59:09 -0300

    node install, css changes and better button generation

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,13 @@ const tipButton = (text) => {
 
 
 const tipButtons = document.getElementById('tip-buttons')
-const tipTexts = ['5%', '10%', '15%', '25%', '50%', 'Custom']
+const customButton = document.getElementById('custom-button')
+const tipValues = ['5', '10', '15', '25', '50']
 
-for (let index = 0; index < tipTexts.length; index++) {
-    const text = tipTexts[index];
+for (let index = 0; index < tipValues.length; index++) {
+    const value = tipValues[index];
     
-    const button = tipButton(text)
+    const button = tipButton(value + '%')
 
-    tipButtons.appendChild(button)
+    tipButtons.insertBefore(button, customButton)
 }
