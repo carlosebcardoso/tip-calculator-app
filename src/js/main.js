4b1a6389349e569baa1b96471fa8a6578dd7065a
Doulpy <carlosebcardoso@gmail.com>
2024-05-09 13:16:26 -0300

    start calculating totals

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,50 @@
+const tipButtons = document.getElementById('tip-buttons')
+const customButton = document.getElementById('custom-button')
+const billInput = document.getElementById('bill-input')
+const peopleInput = document.getElementById('people-input')
+const tipText = document.getElementById('tip-text')
+const totalText = document.getElementById('total-text')
+
+const tipValues = [5, 10, 15, 25, 50]
+
+let tipValue = 0
+let tipAmount = 0
+let total = 0
+
+billInput.addEventListener('input', updateTipAndTotal)
+
+peopleInput.addEventListener('input', updateTipAndTotal)
+
+function updateTipAndTotal () {
+    tipAmount = billInput.value * (tipValue / 100) / peopleInput.value
+    total = billInput.value + tipAmount / peopleInput.value
+
+    tipText.innerText = tipAmount
+    totalText.innerText = total
+}
+
 const tipButton = (text) => {
     const button = document.createElement('button')
     button.classList = "bg-very_dark_cyan text-white py-2 text-xl text-center rounded-md"
     button.innerText = text
 
+    button.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        tipValue = parseInt(button.value)
+        updateTipAndTotal()
+    })
+
     return button
 }
 
 
-const tipButtons = document.getElementById('tip-buttons')
-const customButton = document.getElementById('custom-button')
-const tipValues = ['5', '10', '15', '25', '50']
 
 for (let index = 0; index < tipValues.length; index++) {
-    const value = tipValues[index];
+    const value = tipValues[index]
     
     const button = tipButton(value + '%')
+    button.value = value
 
     tipButtons.insertBefore(button, customButton)
 }
